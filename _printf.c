a518a7b843bafe951d3d08d3ab4d3a4fe45167b5
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-06-27 13:28:14 -0700

    betty

--- _printf.c
+++ _printf.c
@@ -4,29 +4,31 @@
  * _printf - produces output according to a format
  * @format: character string
  *
- * Return: the number of characters printed (excluding the null byte used to end output to strings)
+ * Return: the number of characters printed
+ * (excluding the null byte used to end output to strings)
  */
 
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
+	va_list args;
+	int count = 0;
 
-    va_start(args, format);
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            count += handle_format(&format, args);
-        }
-        else
-        {
-            write(1, format, 1);
-            count++;
-        }
-        format++;
-    }
-    va_end(args);
-    return (count);
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			count += handle_format(&format, args);
+		}
+		else
+		{
+			write(1, format, 1);
+			count++;
+		}
+		format++;
+	}
+	va_end(args);
+
+	return (count);
 }
