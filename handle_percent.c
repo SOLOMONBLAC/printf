e806c39546039a5a48138ee9bc06a8851ffc6661
SOLOMONBLAC <Nwajiobisolo@gmail.com>
2023-06-28 16:20:55 -0700

    binary

--- handle_percent.c
+++ handle_percent.c
@@ -4,12 +4,12 @@
 /**
  * handle_percent - Handle the '%' specifier
  *
+ * @args: Additional arguments or data for processing.
  * Return: Number of characters printed
  */
-
 int handle_percent(va_list args)
 {
-    (void)args;
-    write(1, "%", 1);
-    return (1);
+	(void)args;
+	write(1, "%", 1);
+	return (1);
 }
